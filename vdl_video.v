2107bd415794d5794ac205f96487545d63a79efd
Erdet Nasufi <erdet.nasufi@gmail.com>
2021-10-18 23:32:56 +0200

    Cleaning DisplayEventId enum and removing FlashOperation enum

--- vdl_video.v
+++ vdl_video.v
@@ -257,8 +257,6 @@ pub fn (this WindowEventId)parse(value u32) WindowEventId{
 pub enum DisplayEventId {
 	@none           = C.SDL_DISPLAYEVENT_NONE
 	orientation     = C.SDL_DISPLAYEVENT_ORIENTATION
-	connected       = C.SDL_DISPLAYEVENT_CONNECTED
-	disconnected    = C.SDL_DISPLAYEVENT_DISCONNECTED
 }
 
 pub fn (this DisplayEventId)value() u32 {
@@ -275,20 +273,6 @@ pub enum DisplayOrientation {
 	landscape_flipped = C.SDL_ORIENTATION_LANDSCAPE_FLIPPED
 	portrait          = C.SDL_ORIENTATION_PORTRAIT
 	portrait_flipped  = C.SDL_ORIENTATION_PORTRAIT_FLIPPED
-}
-
-pub enum FlashOperation {
-	cancel            = C.SDL_FLASH_CANCEL
-	briefly           = C.SDL_FLASH_BRIEFLY
-	until_focused     = C.SDL_FLASH_UNTIL_FOCUSED
-}
-
-pub fn (this FlashOperation)value() u32 {
-	return u32(this)
-}
-
-pub fn (mut this FlashOperation)parse(value u32) {
-	this = FlashOperation(value)
 }
 
 pub enum GlConfigAttributes {
@@ -879,69 +863,14 @@ pub enum HitTestResult {
     resize_left        = C.SDL_HITTEST_RESIZE_LEFT
 }
 
-/**
- * Callback used for hit-testing.
- *
- * \param win the SDL_Window where hit-testing was set on
- * \param area an SDL_Point which should be hit-tested
- * \param data what was passed as `callback_data` to SDL_SetWindowHitTest()
- * \return an SDL_HitTestResult value.
- *
- * \sa SDL_SetWindowHitTest
- */
+
 // TODO:
 // typedef SDL_HitTestResult (SDLCALL *SDL_HitTest)(SDL_Window *win,
 // const SDL_Point *area, void *data);
 
-/**
- * Provide a callback that decides if a window region has special properties.
- *
- * Normally windows are dragged and resized by decorations provided by the
- * system window manager (a title bar, borders, etc), but for some apps, it
- * makes sense to drag them from somewhere else inside the window itself; for
- * example, one might have a borderless window that wants to be draggable from
- * any part, or simulate its own title bar, etc.
- *
- * This function lets the app provide a callback that designates pieces of a
- * given window as special. This callback is run during event processing if we
- * need to tell the OS to treat a region of the window specially; the use of
- * this callback is known as "hit testing."
- *
- * Mouse input may not be delivered to your application if it is within a
- * special area; the OS will often apply that input to moving the window or
- * resizing the window and not deliver it to the application.
- *
- * Specifying NULL for a callback disables hit-testing. Hit-testing is
- * disabled by default.
- *
- * Platforms that don't support this functionality will return -1
- * unconditionally, even if you're attempting to disable hit-testing.
- *
- * Your callback may fire at any time, and its firing does not indicate any
- * specific behavior (for example, on Windows, this certainly might fire when
- * the OS is deciding whether to drag your window, but it fires for lots of
- * other reasons, too, some unrelated to anything you probably care about _and
- * when the mouse isn't actually at the location it is testing_). Since this
- * can fire at any time, you should try to keep your callback efficient,
- * devoid of allocations, etc.
- *
- * \param window the window to set hit-testing on
- * \param callback the function to call when doing a hit-test
- * \param callback_data an app-defined void pointer passed to **callback**
- * \returns 0 on success or -1 on error (including unsupported); call
- *          SDL_GetError() for more information.
- *
- * \since This function is available since SDL 2.0.4.
- */
 // TODO
 // extern DECLSPEC int SDLCALL SDL_SetWindowHitTest(SDL_Window * window,
 // SDL_HitTest callback, void *callback_data);
-
-fn C.SDL_FlashWindow(&C.SDL_Window, int) int
-pub fn (this Window)flash(flash_oparation FlashOperation) bool {
-	rc := C.SDL_FlashWindow(this.ptr, int(flash_oparation))
-	return rc == 0
-}
 
 fn C.SDL_DestroyWindow(&C.SDL_Window)
 pub fn (this Window)destroy() {
