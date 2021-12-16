667d80fe7b30a548f1e865df1c8f6b7e805f8595
Erdet Nasufi <erdet.nasufi@gmail.com>
2021-12-17 00:12:50 +0100

    extend [vdl_image.v]: check type of image

--- vdl_image.v
+++ vdl_image.v
@@ -142,14 +142,70 @@ pub fn (this StreamIO) is_lbm() bool {
 }
 
 fn C.IMG_isPCX(&C.SDL_RWops) int
+pub fn (this StreamIO)is_pcx() bool {
+    return C.IMG_isPCX(this.ptr) == 1
+}
+
 fn C.IMG_isPNG(&C.SDL_RWops) int
+pub fn (this StreamIO)is_png() bool {
+    return C.IMG_isPNG(this.ptr) == 1
+}
+
 fn C.IMG_isPNM(&C.SDL_RWops) int
+pub fn (this StreamIO)is_pnm() bool {
+    return C.IMG_isPNM(this.ptr) == 1
+}
+
+/*
 fn C.IMG_isSVG(&C.SDL_RWops) int
+pub fn (this StreamIO)is_svg() bool {
+    return C.IMG_isSVG(this.ptr) == 1
+}
+*/
+
 fn C.IMG_isTIF(&C.SDL_RWops) int
+pub fn (this StreamIO)is_tif() bool {
+    return C.IMG_isTIF(this.ptr) == 1
+}
+
 fn C.IMG_isXCF(&C.SDL_RWops) int
+pub fn (this StreamIO)is_xcf() bool {
+    return C.IMG_isXCF(this.ptr) == 1
+}
+
 fn C.IMG_isXPM(&C.SDL_RWops) int
-fn C.IMG_isXV(&C.SDL_RWops) int
+pub fn (this StreamIO)is_xpm() bool {
+    return C.IMG_isXPM(this.ptr) == 1
+}
+
+fn C.IMG_isXV(&C.SDL_RWops) int 
+pub fn (this StreamIO)is_xv() bool {
+    return C.IMG_isXV(this.ptr) == 1
+}
+
 fn C.IMG_isWEBP(&C.SDL_RWops) int
+pub fn (this StreamIO)is_webp() bool {
+    return C.IMG_isWEBP(this.ptr) == 1
+}
+
+pub fn (this StreamIO)is_type_of(image_type ImageType) bool {
+    return match image_type {
+		.bmp { this.is_bmp() }
+		.cur { this.is_cur() }
+		.gif { this.is_gif() }
+		.ico { this.is_ico() }
+		.jpg { this.is_jpg() }
+		.lbm { this.is_lbm() }
+		.pcx { this.is_pcx() }
+		.png { this.is_png() }
+		.pnm { this.is_pnm() }
+		.tga { false }
+		.tif { this.is_tif() }
+		.xcf { this.is_xcf() }
+		.xpm { this.is_xpm() }
+		.xv  { this.is_xv()  }
+	}
+}
 
 // Individual loading functions
 fn C.IMG_LoadICO_RW(&C.SDL_RWops) &C.SDL_Surface
@@ -170,7 +226,25 @@ fn C.IMG_LoadXV_RW(&C.SDL_RWops) &C.SDL_Surface
 fn C.IMG_LoadWEBP_RW(&C.SDL_RWops) &C.SDL_Surface
 
 // Individual saving functions
+/*
 fn C.IMG_SavePNG(&C.SDL_Surface, &char)
-fn C.IMG_SavePNG_RW(&C.SDL_Surface, &C.SDL_RWops, int)
 fn C.IMG_SaveJPG(&C.SDL_Surface, &char, int)
+pub fn (this Surface)save_png(filename string) {
+    C.IMG_SavePNG(this.ptr, &char(filename.str))
+}
+
+pub fn (this Surface)save_jpg(filename string, quality int) {
+    C.IMG_SaveJPG(this.ptr, &char(filename.str), quality)
+}
+
 fn C.IMG_SaveJPG_RW(&C.SDL_Surface, &C.SDL_RWops, int, int)
+pub fn (this StreamIO)save_jpg(src Surface, quality int) {
+    C.IMG_SaveJPG_RW(src.ptr, this.ptr, 0, quality)
+}
+
+fn C.IMG_SavePNG_RW(&C.SDL_Surface, &C.SDL_RWops, int)
+pub fn (this StreamIO)save_png(src Surface) {
+    C.IMG_SavePNG_RW(src.ptr, this.ptr, 0)
+}
+*/
+
